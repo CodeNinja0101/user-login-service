d6883e779e5c78e1643f92425e969419d71a66ed
CodeNinja0101 <ghumre319@gmail.com>
2024-08-17 01:30:56 +0530

    Initial commit

---
+++ src/main/java/com/github/codeninja0101/user_identity_service/UserIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.codeninja0101.user_identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/codeninja0101/user_identity_service/UserIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.codeninja0101.user_identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
